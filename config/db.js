ddcf918048c75f244664981c4a7712eef38ccab2
Saul Graves <graves.saul@gmail.com>
2021-04-28 15:20:38 -0400

    Connect to Mongo

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('CONNECTED TO MONGO...');
+    } catch (err) {
+        console.error(err.message);
+        // Exit process with failure if connection fails
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
