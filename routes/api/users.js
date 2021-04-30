7cb3cc8c7b0c404b0384c3b6a5051bdebf9a76c0
Saul Graves <graves.saul@gmail.com>
2021-04-30 15:41:32 -0400

    JSON Web Token

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 // @route     POST api/users
@@ -48,7 +50,22 @@ router.post(
             // Save new user
             await newUser.save();
 
-            res.send('User registered.');
+            // Return jwt
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
