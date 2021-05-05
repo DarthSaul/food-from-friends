fde1225f3d3f2f96c3bcd937c56f5e2b1862a60a
Saul Graves <graves.saul@gmail.com>
2021-05-05 15:56:07 -0400

    Profile model

---
+++ models/Profile.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const profileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    favoriteCuisines: {
+        type: [String]
+    },
+    favoriteRestaurants: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            review: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    social: {
+        instagram: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        }
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('Profile', profileSchema);
