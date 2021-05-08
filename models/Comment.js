b3ed2e515d4cdbe5525639c5340885037ec2ceb2
Saul Graves <graves.saul@gmail.com>
2021-05-08 15:27:56 -0400

    List & Comment models

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Comment = mongoose.model('Comment', commentSchema);
--- models/List.js
+++ models/List.js
@@ -1,2 +1,39 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+const listSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    avatar: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    restaurants: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            review: {
+                type: String
+            }
+        }
+    ],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+});
+
+module.exports = List = mongoose.model('List', listSchema);
