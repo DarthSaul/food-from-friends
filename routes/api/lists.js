69bcd0c5171107955ff78e705a86e70f78a84ab6
Saul Graves <graves.saul@gmail.com>
2021-05-10 11:29:49 -0400

    List POST route

--- routes/api/lists.js
+++ routes/api/lists.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route     GET api/lists
-// @desc      Test route
-// @access    Public
-router.get('/', (req, res) => res.send('Lists route'));
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const List = require('../../models/List');
+
+// @route     POST api/lists
+// @desc      Create a list
+// @access    Private
+router.post(
+    '/',
+    auth,
+    check('city', 'City is required').notEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const newList = new List({
+                user: user.id,
+                avatar: user.avatar,
+                city: req.body.city,
+                restaurants: req.body.restaurants
+            });
+            const list = await newList.save();
+            res.json(list);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
 
 module.exports = router;
