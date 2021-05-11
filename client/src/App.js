2e4ad756b4e47f28ad7fae7b9a375073de2d0c24
Saul Graves <graves.saul@gmail.com>
2021-05-11 15:27:02 -0400

    Forgot to branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
 import './App.css';
 
 const App = () => (
-    <>
-        <Navbar />
-        <Landing />
-    </>
+    <Router>
+        <>
+            <Navbar />
+            <Route exact path='/'>
+                <Landing />
+            </Route>
+        </>
+    </Router>
 );
 export default App;
