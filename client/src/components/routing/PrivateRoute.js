b0350e7f618894b30ff1bc1fa967cbaa32dd5e70
Saul Graves <graves.saul@gmail.com>
2021-05-14 12:13:52 -0400

    Private routing

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -2,24 +2,6 @@ import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 
-// const PrivateRoute = ({ component: Component, ...rest }) => {
-//     const {
-//         userObj: { isAuthenticated, loading }
-//     } = useContext(UserContext);
-//     return (
-//         <Route
-//             {...rest}
-//             render={props =>
-//                 !isAuthenticated && !loading ? (
-//                     <Redirect to='/login' />
-//                 ) : (
-//                     <Component {...props} />
-//                 )
-//             }
-//         />
-//     );
-// };
-
 const PrivateRoute = ({ children, ...rest }) => {
     const {
         userObj: { isAuthenticated, loading }
