f28c35012f43ddef621cf31c82cbb7f932d81870
Saul Graves <graves.saul@gmail.com>
2021-05-20 18:23:13 -0400

    Refactor routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import EditProfile from './components/profile-forms/EditProfile';
-import AddRestaurant from './components/profile-forms/AddRestaurant';
-import AddMedia from './components/profile-forms/AddMedia';
-import Lists from './components/lists/Lists';
-import ListForm from './components/list-form/ListForm';
-import List from './components/list/List';
-
-import PrivateRoute from './components/routing/PrivateRoute';
+import Routes from './components/routing/Routes';
 
 import { UserContext } from './contexts/UserContext';
 import setAuthToken from './utils/setAuthToken';
@@ -39,52 +25,14 @@ const App = () => {
         <Router>
             <>
                 <Navbar />
-                <Route exact path='/'>
-                    <Landing />
-                </Route>
-                <section className='container'>
-                    <Alert />
-                    <Switch>
-                        {/* Public Routes */}
-                        <Route exact path='/register'>
-                            <Register />
-                        </Route>
-                        <Route exact path='/login'>
-                            <Login />
-                        </Route>
-                        <Route exact path='/profiles'>
-                            <Profiles />
-                        </Route>
-                        <Route exact path='/profile/:id'>
-                            <Profile />
-                        </Route>
-                        <Route exact path='/lists'>
-                            <Lists />
-                        </Route>
-                        <Route exact path='/lists/new'>
-                            <ListForm />
-                        </Route>
-                        <Route exact path='/lists/:id'>
-                            <List />
-                        </Route>
-                        {/* Private Routes */}
-                        <PrivateRoute exact path='/dashboard'>
-                            <Dashboard />
-                        </PrivateRoute>
-                        <PrivateRoute exact path='/create-profile'>
-                            <CreateProfile />
-                        </PrivateRoute>
-                        <PrivateRoute exact path='/edit-profile'>
-                            <EditProfile />
-                        </PrivateRoute>
-                        <PrivateRoute exact path='/add-restaurant'>
-                            <AddRestaurant />
-                        </PrivateRoute>
-                        <PrivateRoute exact path='/add-media'>
-                            <AddMedia />
-                        </PrivateRoute>
-                    </Switch>
-                </section>
+                <Switch>
+                    <Route exact path='/'>
+                        <Landing />
+                    </Route>
+                    <Route>
+                        <Routes />
+                    </Route>
+                </Switch>
             </>
         </Router>
     );
