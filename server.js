d9b8c8f35c6c3cfeea6fdfa44c0dff49cfdd0194
Saul Graves <graves.saul@gmail.com>
2021-09-21 09:26:06 -0400

    deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 connectDB();
 
-app.use(express.json({ extended: true }));
+app.use(express.json());
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
