95a84dd261eccedfbd34071d1b9262b551641d93
Saul Graves <graves.saul@gmail.com>
2021-10-04 17:54:50 -0400

    Footer GitHub link

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
+import Footer from './components/layout/Footer';
 
 import './App.css';
 
@@ -19,6 +20,7 @@ const App = () => (
                     <Routes />
                 </Route>
             </Switch>
+            <Footer />
         </>
     </Router>
 );
