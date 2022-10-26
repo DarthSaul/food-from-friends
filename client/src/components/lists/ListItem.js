db9226319a6930343db78842a68f514b722980ad
Saul Graves <graves.saul@gmail.com>
2022-10-26 15:34:02 -0400

    lists container

--- client/src/components/lists/ListItem.js
+++ client/src/components/lists/ListItem.js
@@ -3,93 +3,113 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faThumbsUp,
-    faThumbsDown,
-    faTimes
+	faThumbsUp,
+	faThumbsDown,
+	faTimes,
 } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 import { UserContext } from '../../contexts/UserContext';
 import { ListsContext } from '../../contexts/ListsContext';
 
 const ListItem = ({
-    list: {
-        _id,
-        city,
-        likes,
-        comments,
-        user: { name, avatar, _id: user_id }
-    },
-    showActions
+	list: {
+		_id,
+		city,
+		likes,
+		comments,
+		user: { name, avatar, _id: user_id },
+	},
+	showActions,
 }) => {
-    const {
-        userObj: { loading: userLoading, user: currentUser }
-    } = useContext(UserContext);
-    const { likeList, unlikeList, deleteList } = useContext(ListsContext);
-    return (
-        <div className='list bg-success p-1 my-1'>
-            <img src={avatar.url} alt='' />
-            <div className='list-inner'>
-                <p className='lead text-black'>{city}</p>
-                <h4>
-                    A list by{' '}
-                    <Link
-                        to={`/profile/${user_id}`}
-                        className='list-profile-link'
-                    >
-                        {name}
-                    </Link>
-                </h4>
-                {showActions && (
-                    <div className='list-actions'>
-                        <Link to={`/lists/${_id}`} className='btn btn-primary'>
-                            View List
-                        </Link>
-                        <button
-                            type='button'
-                            className='btn btn-light'
-                            onClick={() => likeList(_id)}
-                        >
-                            <FontAwesomeIcon icon={faThumbsUp} />{' '}
-                            {likes.length > 0 && <span>{likes.length}</span>}
-                        </button>
+	const {
+		userObj: { loading: userLoading, user: currentUser },
+	} = useContext(UserContext);
+	const { likeList, unlikeList, deleteList } = useContext(ListsContext);
+	return (
+		<div className="list mb-1">
+			<div className="list-top p-1">
+				<div className="list-inner-header">
+					<div>
+						<div className="fs-2">{city}</div>
+						<div className="fw-light">
+							<span className="mr">{name}</span>
+							<Link
+								to={`/profile/${user_id}`}
+								className="list-profile-link"
+							>
+								<FontAwesomeIcon
+									icon={faUserCircle}
+									size="md"
+								/>
+							</Link>
+						</div>
+					</div>
+					<div>
+						<img src={avatar.url} alt="" />
+					</div>
+				</div>
+				<div>
+					<Link to={`/lists/${_id}`} className="btn btn-primary">
+						View List
+					</Link>
+				</div>
+			</div>
 
-                        <button
-                            type='button'
-                            className='btn btn-danger'
-                            onClick={() => unlikeList(_id)}
-                        >
-                            <FontAwesomeIcon icon={faThumbsDown} />
-                        </button>
+			<hr />
+			{showActions && (
+				<div className="list-actions p-1">
+					<div>
+						<button
+							type="button"
+							className="btn btn-dark btn-sm mr"
+							onClick={() => likeList(_id)}
+						>
+							<FontAwesomeIcon icon={faThumbsUp} />{' '}
+							{likes.length > 0 && <span>{likes.length}</span>}
+						</button>
 
-                        <Link to={`/lists/${_id}`} className='btn btn-dark'>
-                            Discussion{' '}
-                            {comments.length > 0 && (
-                                <span className='comment-count'>
-                                    {comments.length}
-                                </span>
-                            )}
-                        </Link>
+						<button
+							type="button"
+							className="btn btn-dark btn-sm mr"
+							onClick={() => unlikeList(_id)}
+						>
+							<FontAwesomeIcon icon={faThumbsDown} />
+						</button>
 
-                        {!userLoading &&
-                            currentUser &&
-                            user_id === currentUser.data._id && (
-                                <button
-                                    type='button'
-                                    className='btn btn-danger'
-                                    onClick={() => deleteList(_id)}
-                                >
-                                    <FontAwesomeIcon icon={faTimes} />
-                                </button>
-                            )}
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+						<Link
+							to={`/lists/${_id}`}
+							className="btn btn-dark btn-sm"
+						>
+							Discussion{' '}
+							{comments.length > 0 && (
+								<span className="comment-count">
+									{comments.length}
+								</span>
+							)}
+						</Link>
+					</div>
+					<div>
+						{!userLoading &&
+							currentUser &&
+							user_id === currentUser.data._id && (
+								<button
+									type="button"
+									className="btn btn-danger btn-sm"
+									onClick={() => deleteList(_id)}
+								>
+									<FontAwesomeIcon icon={faTimes} />
+								</button>
+							)}
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 ListItem.propTypes = {
-    list: PropTypes.object.isRequired
+	list: PropTypes.object.isRequired,
 };
 
 export default ListItem;
--- client/src/components/lists/Lists.js
+++ client/src/components/lists/Lists.js
@@ -1,55 +1,60 @@
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHotdog } from '@fortawesome/free-solid-svg-icons';
-
 import { ListsContext } from '../../contexts/ListsContext';
-
 import Spinner from '../layout/Spinner';
 import ListItem from './ListItem';
+import '../../css/Lists.css';
 
 const Lists = () => {
-    const {
-        getLists,
-        listState: { lists, loading }
-    } = useContext(ListsContext);
+	const {
+		getLists,
+		listState: { lists, loading },
+	} = useContext(ListsContext);
 
-    useEffect(() => {
-        getLists();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+	useEffect(() => {
+		getLists();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-    return (
-        <>
-            {loading ? (
-                <Spinner />
-            ) : (
-                <>
-                    <h1 className='x-large text-primary my-1'>Lists</h1>
-                    <p className='lead mb-2'>
-                        <FontAwesomeIcon icon={faHotdog} /> Welcome to the
-                        community
-                    </p>
-                    <Link to={'/lists/new'} className='btn btn-primary mb-2'>
-                        <span className='fs-2'>Create a new list</span>
-                    </Link>
-                    <div className='lists'>
-                        {lists.length > 0 ? (
-                            lists.map(list => (
-                                <ListItem
-                                    key={list._id}
-                                    list={list}
-                                    showActions={true}
-                                />
-                            ))
-                        ) : (
-                            <h4>No lists found.</h4>
-                        )}
-                    </div>
-                </>
-            )}
-        </>
-    );
+	return (
+		<>
+			{loading ? (
+				<Spinner />
+			) : (
+				<>
+					<div className="lists-header mb-2">
+						<div>
+							<h1 className="large text-primary">Lists</h1>
+							<p className="fw-light fs-1">
+								View curated lists from the community.
+							</p>
+						</div>
+						<div>
+							<Link to={'/lists/new'} className="btn btn-dark">
+								<span className="fs-1">Create a new list</span>
+							</Link>
+						</div>
+					</div>
+
+					<hr className="mb-2" />
+
+					<div className="lists">
+						{lists.length > 0 ? (
+							lists.map((list) => (
+								<ListItem
+									key={list._id}
+									list={list}
+									showActions={true}
+								/>
+							))
+						) : (
+							<h4>No lists found.</h4>
+						)}
+					</div>
+				</>
+			)}
+		</>
+	);
 };
 
 export default Lists;
